68cab7bfdebd01939956b9dda201a67231675258
Stephanie Melnyk <smelnyk@vendasta.com>
2015-12-17 10:36:54 -0600

    Advent of Code - Day 4  - Solved Part 1 and Part 2 with appropriate tests. Updated docstrings for methods as well and tidies up the code.

--- Day 04/day4_code.py
+++ Day 04/day4_code.py
@@ -2,65 +2,77 @@ __author__ = 'smelnyk'
 
 import md5
 
-"""
-Santa needs help mining some AdventCoins (very similar to bitcoins) to use
-as gifts for all the economically forward-thinking little girls and boys.
 
-* To do this, he needs to find MD5 hashes which, in hexadecimal, start with
-  at least five zeroes.
+def mine_bitcoins_part1(secret_key):
+    """
+    Santa needs help mining some AdventCoins (very similar to bitcoins) to use
+    as gifts for all the economically forward-thinking little girls and boys.
 
-* The input to the MD5 hash is some secret key (your puzzle input, given
-  below) followed by a number in decimal.
+    * To do this, he needs to find MD5 hashes which, in hexadecimal, starts with
+      at least five zeroes.
 
-* To mine AdventCoins, you must find Santa the lowest positive number
-  (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+    * The input to the MD5 hash is some secret key (your puzzle input, given
+      below) followed by a number in decimal.
 
-For example:
+    * To mine AdventCoins, you must find Santa the lowest positive number
+      (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+    """
 
-If your secret key is abcdef, the answer is 609043, because the MD5 hash of
-abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest
-such number to do so.
-
-If your secret key is pqrstuv, the lowest number it combines with to make an
-MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of
-pqrstuv1048970 looks like 000006136ef....
-
-Your puzzle input is: yzbqklnj
-"""
-
-
-def mine_bitcoins(secret_key):
-
-    print "secret_key is: " + str(secret_key)
+    # print "secret_key is: " + str(secret_key)
 
     hash_answer = ''
     hash_hexdigest = ''
-
     secret_key_updated = ''
-
-    md5_hash = md5.new()
-
-    # Time to find the lowest number the hex combines with to make a MD5
-    # hash starting with five zeros
-    # Keep regenerating the hash until we get one with 5 leading zeros
-        # return the number that produced a has with the 5 leading zeros?
 
     for x in range(0, 1048971):
 
-        # Need to append the number at the end of the string and then update that, not 2 updates
         secret_key_updated = secret_key + str(x)
         # print "secret_key_updated is: " + str(secret_key_updated)
 
+        md5_hash = md5.new()
         md5_hash.update(secret_key_updated)
-
-        # print "md5_hash is: " + str(md5_hash)
-
         hash_hexdigest = md5_hash.hexdigest()
         hash_answer = hash_hexdigest
-
         # print "Current hash_answer is: " + str(hash_answer)
 
         if hash_answer.startswith('00000'):
+            print "Current hash_answer is: " + str(hash_answer)
+            return x
+
+
+def mine_bitcoins_part2(secret_key):
+    """
+    Santa needs help mining some AdventCoins (very similar to bitcoins) to use
+    as gifts for all the economically forward-thinking little girls and boys.
+
+    * To do this, he needs to find MD5 hashes which, in hexadecimal, starts with
+      at least six zeroes.
+
+    * The input to the MD5 hash is some secret key (your puzzle input, given
+      below) followed by a number in decimal.
+
+    * To mine AdventCoins, you must find Santa the lowest positive number
+      (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+    """
+
+    # print "secret_key is: " + str(secret_key)
+
+    hash_answer = ''
+    hash_hexdigest = ''
+    secret_key_updated = ''
+
+    for x in range(0, 10000000):
+
+        secret_key_updated = secret_key + str(x)
+        # print "secret_key_updated is: " + str(secret_key_updated)
+
+        md5_hash = md5.new()
+        md5_hash.update(secret_key_updated)
+        hash_hexdigest = md5_hash.hexdigest()
+        hash_answer = hash_hexdigest
+        # print "Current hash_answer is: " + str(hash_answer)
+
+        if hash_answer.startswith('000000'):
             print "Current hash_answer is: " + str(hash_answer)
             return x
 
--- Day 04/day4_test.py
+++ Day 04/day4_test.py
@@ -1,22 +1,43 @@
 __author__ = 'smelnyk'
 
 import unittest
-from day4_code import mine_bitcoins
+from day4_code import mine_bitcoins_part1, mine_bitcoins_part2
 
 
 class Day4Part1Tests(unittest.TestCase):
 
     def test_first_test_example_given(self):
         secret_key = "abcdef"
-        result = mine_bitcoins(secret_key)
+        result = mine_bitcoins_part1(secret_key)
         self.assertEqual(609043, result)
 
     def test_second_test_example_given(self):
         secret_key = "pqrstuv"
-        result = mine_bitcoins(secret_key)
+        result = mine_bitcoins_part1(secret_key)
         self.assertEqual(1048970, result)
 
-    # def test_solve_day4_part1(self):
-    #     secret_key = "yzbqklnj"
-    #     result = mine_bitcoins(secret_key)
-    #     print "The Answer to Day 4, Part 1 is: " + str(result)
+    def test_solve_day4_part1(self):
+        secret_key = "yzbqklnj"
+        result = mine_bitcoins_part1(secret_key)
+        print "The Answer to Day 4, Part 1 is: " + str(result) #Answer: 282749
+
+
+class Day4Part2Tests(unittest.TestCase):
+
+    def test_first_test_example_given(self):
+        secret_key = "abcdef"
+        result = mine_bitcoins_part2(secret_key)
+        # print "The Answer is: " + str(result) # Answer: 6742839
+        self.assertEqual(6742839, result)
+
+    def test_second_test_example_given(self):
+        secret_key = "pqrstuv"
+        result = mine_bitcoins_part2(secret_key)
+        # print "The Answer is: " + str(result) # Answer: 5714438
+        self.assertEqual(5714438, result)
+
+    def test_solve_day4_part2(self):
+        secret_key = "yzbqklnj"
+        result = mine_bitcoins_part2(secret_key)
+        # print "The Answer to Day 4, Part 2 is: " + str(result) # Answer: 9962624
+        self.assertEqual(9962624, result)
