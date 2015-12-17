41ed2e5083f2e32d4fdf9d1f3142650a35375aa5
Stephanie Melnyk <smelnyk@vendasta.com>
2015-12-17 11:01:22 -0600

    Advent of Code - Day 5 Part 1 - Got checking for if a string has three vowels working, includes tests

--- Day 05/day5_code.py
+++ Day 05/day5_code.py
@@ -1,1 +1,45 @@
 __author__ = 'smelnyk'
+
+
+def find_number_strings_that_are_nice(strings):
+    """
+    Santa needs help figuring out which strings in his text file are naughty or nice.
+
+    A nice string is one with all of the following properties:
+
+    * It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+    * It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or
+      aabbccdd (aa, bb, cc, or dd).
+    * It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the
+      other requirements.
+
+    :param strings: a list of strings to check against
+    :return: num of strings matching the "nice" criteria
+    """
+
+    num_of_vowels = 0
+    num_of_nice_strings = 0
+
+    # loop over each character in the string
+    for character in strings:
+        # if three vowels found (aeiou)
+        if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
+            # incrememt num_of_vowels to be 1
+            num_of_vowels += 1
+
+    # If the string has at least three vowels
+    if num_of_vowels >= 3:
+        num_of_nice_strings += 1
+
+    # check if the string contains at least one letter that appears twice in a row, like xx, abcdde (dd), or
+    # aabbccdd (aa, bb, cc, or dd).
+
+    # check that it doesn't contain the following strings(ab, cd, pq, or xy)
+    # even if they are part of one of the other requirements.
+
+    if num_of_vowels >= 3:
+        num_of_nice_strings += 1
+
+    # return num_of_nice_words
+    print "num_of_nice_strings is: " + str(num_of_nice_strings)
+    return num_of_nice_strings
--- Day 05/day5_test.py
+++ Day 05/day5_test.py
@@ -1,3 +1,22 @@
 __author__ = 'smelnyk'
 
 import unittest
+from day5_code import find_number_strings_that_are_nice
+
+class Day5Part1Tests(unittest.TestCase):
+
+    def test_contains_at_least_three_vowels(self):
+        result = find_number_strings_that_are_nice('aei')
+        self.assertEqual(1, result)
+
+    def test_contains_at_least_three_vowels_two(self):
+        result = find_number_strings_that_are_nice('xazegov')
+        self.assertEqual(1, result)
+
+    def test_contains_at_least_three_vowels_three(self):
+        result = find_number_strings_that_are_nice('aeiouaeiouaeiou')
+        self.assertEqual(1, result)
+
+    def test_does_not_contain_at_least_three_vowels(self):
+        result = find_number_strings_that_are_nice('hello')
+        self.assertEqual(0, result)
