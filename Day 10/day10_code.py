fdb05536143fc9249ba12c97e76ba4b89a38cf1a
Stephanie Melnyk <smelnyk@vendasta.com>
2015-12-21 09:30:10 -0600

    Advent of Code - Day 6 Part 1 - Brute forcing so far. Time to make it loop through.

--- Day 10/day10_code.py
+++ Day 10/day10_code.py
@@ -1,1 +1,75 @@
 __author__ = 'smelnyk'
+
+"""
+Today, the Elves are playing a game called look-and-say.
+They take turns making sequences by reading aloud the previous sequence and
+using that reading as the next sequence.
+
+For example:
+211 is read as "one two, two ones", which becomes 1221 (1 2, 2 1s).
+
+Look-and-say sequences are generated iteratively, using the previous value
+as input for the next step.
+
+For each step, take the previous value, and replace each run of digits
+(like 111) with the number of digits (3) followed by the digit itself (1).
+
+For example:
+1 becomes 11 (1 copy of digit 1).
+11 becomes 21 (2 copies of digit 1).
+21 becomes 1211 (one 2 followed by one 1).
+1211 becomes 111221 (one 1, one 2, and two 1s).
+111221 becomes 312211 (three 1s, two 2s, and one 1).
+
+Starting with the digits in your puzzle input, apply this process 40 times.
+What is the length of the result?
+"""
+
+
+def get_total_length(input_number):
+
+    print "input_number is: " + str(input_number)
+
+    new_number = ""
+    num_of_copies_of_number = 0
+
+    if input_number == 1:
+
+        single_digit_1 = 1
+
+        num_of_copies_of_number += input_number
+        print "num_of_copies_of_number is: " + str(num_of_copies_of_number)
+
+        new_number = str(num_of_copies_of_number) + str(single_digit_1)
+        print "new_number is: " + str(new_number)
+
+    if input_number == 11:
+
+        single_digit_1 = 1
+
+        num_of_copies_of_number = (input_number + input_number) / input_number
+        print "num_of_copies_of_number is: " + str(num_of_copies_of_number)
+
+        new_number = str(num_of_copies_of_number) + str(single_digit_1)
+        print "new_number is: " + str(new_number)
+
+    if input_number == 21:
+
+        single_digit_1 = 2
+        single_digit_2 = 1
+
+        if single_digit_1 == single_digit_2:
+            num_of_copies_of_number = (input_number + input_number) / input_number
+            print "num_of_copies_of_number is: " + str(num_of_copies_of_number)
+        else:
+            num_of_copies_of_number += 1
+            print "num_of_copies_of_number is: " + str(num_of_copies_of_number)
+
+        new_number = str(single_digit_2) + str(single_digit_1) + str(num_of_copies_of_number) + str(single_digit_2)
+        print "new_number is: " + str(new_number)
+
+    print
+    return int(new_number)
+
+
+
--- Day 10/day10_test.py
+++ Day 10/day10_test.py
@@ -1,3 +1,19 @@
 __author__ = 'smelnyk'
 
 import unittest
+from day10_code import get_total_length
+
+
+class Day10Part1Tests(unittest.TestCase):
+
+    def test_first_example_should_return_11(self):
+        result = get_total_length(1)
+        self.assertEqual(11, result)
+
+    def test_second_example_should_return_21(self):
+        result = get_total_length(11)
+        self.assertEqual(21, result)
+
+    def test_third_example_should_return_1211(self):
+        result = get_total_length(21)
+        self.assertEqual(1211, result)
