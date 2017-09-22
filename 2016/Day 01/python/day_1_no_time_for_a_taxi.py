4c806ea20e3e7217f837d9ce9a81c1b6917f20ac
smelnyk-va <smelnyk@vendasta.com>
2017-09-22 14:27:29 -0600

    Trying to get tests to run locally... Day 1 stubs

---
+++ 2016/Day 01/python/day_1_no_time_for_a_taxi.py
@@ -0,0 +1,9 @@
+""" Day 1 Advent of Code 2016 - Python Solution Attempt """
+
+
+class DirectionCalculator():
+
+    @staticmethod
+    def direction_calculator():
+        starting_position = (0, 0)
+        return starting_position
